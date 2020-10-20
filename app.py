35f6690de20b626ad2920fe20d4964096999fd51
vadyme <vadym.rudenko@gmail.com>
2020-10-19 22:06:07 -0700

    adding a primitive menu

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import standing_table
 import fixture, matchday
 
@@ -7,17 +7,28 @@ app = Flask(__name__)
 json_file = './models/standing_table.json'
 fixtures_json = './models/fixtures_by_league.json'
 
-@app.route('/')
+# league_id = 2833
+
+
+@app.route('/', methods=['GET'])
 def index():
-    return standing_table.build_table(2833)
+    return render_template('index.html')
+    # return standing_table.build_table(league_id)
 
-@app.route('/fixtures')
-def fixtures():
-    return fixture.build_table(2833)
 
-@app.route('/current')
-def current():
-    return matchday.build_table(2833)
+@app.route('/standings-table/<int:league_id>', methods=['GET'])
+def standings_table(league_id):
+    return standing_table.build_table(league_id)
+
+
+@app.route('/fixtures/<int:league_id>')
+def fixtures(league_id):
+    return fixture.build_table(league_id)
+
+
+@app.route('/current/<int:league_id>')
+def current(league_id):
+    return matchday.build_table(league_id)
 
 
 if __name__ == "__main__":
