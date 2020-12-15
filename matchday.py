a5b8a030fdf33b61ae02b428f5d578345160ffad
vadyme <vadym.rudenko@gmail.com>
2020-12-14 22:50:08 -0800

    slight code refactoring

--- matchday.py
+++ matchday.py
@@ -47,19 +47,19 @@ def get_fixtures_by_date(date):
     # TODO: if there are multiple fixtures as above, think on sending asynchronous calls
     now = time.time()
     for f in fs:
-        if f.timestamp < now and f.status != 'Match Finished':
+        if f.timestamp < now and (f.status != 'Match Finished'):
             print(f' "The game needs an update: " {f.id}')
             fixture_update = api_client.get_fixture_by_id(f.id)
-            # f.score = fixture_update['api']['fixtures'][0]['score']['fulltime']
-            f.status_short = fixture_update['api']['fixtures'][0]['statusShort']
-            f.status = fixture_update['api']['fixtures'][0]['status']
-            f.elapsed = fixture_update['api']['fixtures'][0]['elapsed']
-            f.goals_home_team = fixture_update['api']['fixtures'][0]['goalsHomeTeam']
-            f.goals_away_team = fixture_update['api']['fixtures'][0]['goalsAwayTeam']
-            f.score_ht = fixture_update['api']['fixtures'][0]['score']['halftime']
-            f.score_ft = fixture_update['api']['fixtures'][0]['score']['fulltime']
-            f.score_et = fixture_update['api']['fixtures'][0]['score']['extratime']
-            f.score_pen = fixture_update['api']['fixtures'][0]['score']['penalty']
+            update = fixture_update['api']['fixtures'][0]
+            f.status_short = update['statusShort']
+            f.status = update['status']
+            f.elapsed = update['elapsed']
+            f.goals_home_team = update['goalsHomeTeam']
+            f.goals_away_team = update['goalsAwayTeam']
+            f.score_ht = update['halftime']
+            f.score_ft = update['fulltime']
+            f.score_et = update['extratime']
+            f.score_pen = update['penalty']
             # TODO: rework the fixture class, add all necessary fields; make sure to update all fields in DB
             dao.update_fixture_object(f.id, f.status_short, f.status, f.elapsed, f.goals_home_team, f.goals_away_team,
                                       f.score_ht, f.score_ft, f.score_et, f.score_pen)
