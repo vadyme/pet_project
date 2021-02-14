f852a287360278f09710035946c009cfddcc630e
vadyme <vadym.rudenko@gmail.com>
2021-02-13 22:16:26 -0800

    async API requests + write_bunch to DB

--- dao.py
+++ dao.py
@@ -1,5 +1,7 @@
 from db import db
 import logging
+import time
+from datetime import datetime
 from bson.json_util import dumps
 
 
@@ -47,14 +49,42 @@ def update_fixture_object(event_date, event_timestamp, fixture_id, status_short,
     return data
 
 
+def update_multiple_fixtures(q):
+    print(f' "Writing bulk @ {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))}')
+    data = db.fixtures_db_collection.bulk_write(q)
+    print(f' "Writing bulk complete @ {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))}')
+    return data
+
+
 # def get_form_by_team(team_id, league_id):
 #     q = {"$and": [{"$or": [{"homeTeam.team_id": team_id}, {"awayTeam.team_id": team_id}]}, {"league_id": league_id}, {"status" :"Match Finished"}]}
 #     data = list(db.fixtures_db_collection.find(q).sort('event_timestamp', -1).limit(5))
 #
 #     return data
 
+# def get_form_by_team(league_id, team_id):
+#     q = {"$and": [{"league_id": league_id}, {"status": "Match Finished"}, {"$or": [{"homeTeam.team_id": team_id}, {"awayTeam.team_id": team_id}]}]}
+#     data = list(db.fixtures_db_collection.find(q).sort('event_timestamp', -1).limit(5))
+#
+#     return data
+
 def get_form_by_team(team_id):
-    q = {"$and": [{"$or": [{"homeTeam.team_id": team_id}, {"awayTeam.team_id": team_id}]}, {"status" :"Match Finished"}]}
+    q = {"$and": [{"status": "Match Finished"}, {"$or": [{"homeTeam.team_id": team_id}, {"awayTeam.team_id": team_id}]}]}
     data = list(db.fixtures_db_collection.find(q).sort('event_timestamp', -1).limit(5))
 
     return data
+
+
+# def get_form_by_team(league_id, team_ids):
+#
+#     print(datetime.now())
+#     team_form = []
+#
+#     for team_id in team_ids:
+#         q = {"$and": [{"league_id": league_id}, {"status": "Match Finished"}, {"$or": [{"homeTeam.team_id": team_id}, {"awayTeam.team_id": team_id}]}]}
+#         data = list(db.fixtures_db_collection.find(q).sort('event_timestamp', -1).limit(5))
+#
+#         team_form.append((team_id, data))
+#
+#     print(datetime.now())
+#     return team_form
--- matchday.py
+++ matchday.py
@@ -5,6 +5,7 @@ import asyncio
 import api_client
 import fixture
 import dao
+from pymongo import UpdateOne
 
 
 def get_current_matchday_id(league_id):
@@ -36,7 +37,8 @@ def get_fixtures_by_league_and_round(league_id, matchday_id):
     # fixture_data = get_fixture_data(league_id)
     fixtures = dao.get_fixtures_by_league_and_round(league_id, matchday_id)
     fs = build_list_of_fixture_objects(fixtures)
-    update_if_live(fs)
+    # update_if_live(fs)
+    asyncio.run(async_update_if_live(fs))
 
     return sorted(fs, key=lambda x: (x.kickoff_date.date, x.kickoff_date.time))
 
@@ -54,66 +56,72 @@ def get_fixtures_by_date(date):
     return sorted_fixtures
 
 
-def update_if_live(fs):
-    now = time.time()
-    for f in fs:
-        if f.timestamp < now and (f.status != 'Match Finished') or f.status_short == 'TBD':
-            print(f' "The game needs an update: " {f.id} @ {time.time()}')
-            fixture_update = api_client.get_fixture_by_id(f.id)
-            update = fixture_update['api']['fixtures'][0]
-            update_score = update['score']
-            f.event_date = update['event_date']
-            f.event_timestamp = update['event_timestamp']
-            f.status = update['status']
-            f.status_short = update['statusShort']
-            f.status = update['status']
-            f.elapsed = update['elapsed']
-            f.goals_home_team = update['goalsHomeTeam']
-            f.goals_away_team = update['goalsAwayTeam']
-            f.score_ht = update_score['halftime']
-            f.score_ft = update_score['fulltime']
-            f.score_et = update_score['extratime']
-            f.score_pen = update_score['penalty']
-            # TODO: rework the fixture class, add all necessary fields; make sure to update all fields in DB
-            dao.update_fixture_object(f.event_date, f.event_timestamp, f.id, f.status_short, f.status, f.elapsed, f.goals_home_team, f.goals_away_team,
-                                      f.score_ht, f.score_ft, f.score_et, f.score_pen)
-            print(f' "The game has been updated: " {f.id} @ {time.time()}')
+# def update_if_live(fs):
+#     now = time.time()
+#     for f in fs:
+#         if f.timestamp < now and (f.status != 'Match Finished') or f.status_short == 'TBD':
+#             print(f' "The game needs an update: " {f.id} @ {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))}')
+#             fixture_update = api_client.get_fixture_by_id(f.id)
+#             update = fixture_update['api']['fixtures'][0]
+#             update_score = update['score']
+#             f.event_date = update['event_date']
+#             f.event_timestamp = update['event_timestamp']
+#             f.status = update['status']
+#             f.status_short = update['statusShort']
+#             f.status = update['status']
+#             f.elapsed = update['elapsed']
+#             f.goals_home_team = update['goalsHomeTeam']
+#             f.goals_away_team = update['goalsAwayTeam']
+#             f.score_ht = update_score['halftime']
+#             f.score_ft = update_score['fulltime']
+#             f.score_et = update_score['extratime']
+#             f.score_pen = update_score['penalty']
+#             # TODO: rework the fixture class, add all necessary fields; make sure to update all fields in DB
+#             dao.update_fixture_object(f.event_date, f.event_timestamp, f.id, f.status_short, f.status, f.elapsed,
+#                                       f.goals_home_team, f.goals_away_team,
+#                                       f.score_ht, f.score_ft, f.score_et, f.score_pen)
+#             print(f' "The game has been updated: " {f.id} @ {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))}')
 
 
 async def async_update_if_live(fs):
     now = time.time()
     async with aiohttp.ClientSession() as session:
-        updates = []
+        tasks = []
+        #TODO: check if live upstream to avoid iterating through the entire list here
         for f in fs:
             if f.timestamp < now and (f.status != 'Match Finished') or f.status_short == 'TBD':
-                print(f' "The game needs an update: " {f.id} @ {time.time()}')
-                update = asyncio.ensure_future(update_fixture(session, f))
-                updates.append(update)
+                print(f' "The game needs an update: " {f.id} @ {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))}')
+                task = asyncio.ensure_future(get_fixture_update(session, f))
+                tasks.append(task)
+        # list of queries:
+        query = await asyncio.gather(*tasks)
+        print(f' "Updating the DB: @ {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))}')
+        if len(query) > 0:
+            dao.update_multiple_fixtures(query)
+            print(f' "DB updated: @ {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))}')
 
-        return await asyncio.gather(*updates)
 
-
-async def update_fixture(session, f):
-    fixture_update = api_client.get_fixture_by_id(f.id)
-    update = fixture_update['api']['fixtures'][0]
-    update_score = update['score']
-    f.event_date = update['event_date']
-    f.event_timestamp = update['event_timestamp']
-    f.status = update['status']
-    f.status_short = update['statusShort']
-    f.status = update['status']
-    f.elapsed = update['elapsed']
-    f.goals_home_team = update['goalsHomeTeam']
-    f.goals_away_team = update['goalsAwayTeam']
-    f.score_ht = update_score['halftime']
-    f.score_ft = update_score['fulltime']
-    f.score_et = update_score['extratime']
-    f.score_pen = update_score['penalty']
-    # TODO: rework the fixture class, add all necessary fields; make sure to update all fields in DB
-    dao.update_fixture_object(f.event_date, f.event_timestamp, f.id, f.status_short, f.status, f.elapsed,
-                              f.goals_home_team, f.goals_away_team,
-                              f.score_ht, f.score_ft, f.score_et, f.score_pen)
-    print(f' "The game has been updated: " {f.id} @ {time.time()}')
+async def get_fixture_update(session, f):
+    headers = {'X-RapidAPI-Key': api_client.API_KEY}
+    print(f' "Requsting fixture update from API @ {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))}')
+    async with session.get(f'https://api-football-v1.p.rapidapi.com/v2/fixtures/id/{f.id}', headers=headers) as response:
+        resdata = await response.json()
+        update = resdata['api']['fixtures'][0]
+        query = UpdateOne({'fixture_id': f.id}, {"$set": {'statusShort': update['statusShort'],
+                                                          'status': update['status'],
+                                                          'elapsed': update['elapsed'],
+                                                          'goalsHomeTeam': update['goalsHomeTeam'],
+                                                          'goalsAwayTeam': update['goalsAwayTeam'],
+                                                          'score.halftime': update['score']['halftime'],
+                                                           'score.fulltime': update['score']['fulltime'],
+                                                          'score.extratime': update['score']['extratime'],
+                                                          'score.penalty': update['score']['penalty'],
+                                                          'event_date': update['event_date'],
+                                                          'event_timestamp': update['event_timestamp']
+                                                          }
+                                                 })
+        print(f' "Fixture update query ready @ {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))}')
+    return query
 
 
 def build_list_of_fixture_objects(fixtures):
